119c3f0a7f9781eca17158093f784b4c03553a20
thomas huan <thomas.huan@beacora.com>
2021-10-26 10:12:47 +0800

    interface and customer type

---
+++ ch11/interface/interface_test.go
@@ -0,0 +1,24 @@
+package interface_test
+
+import "testing"
+
+type Programmer interface {
+	WriteHelloWorld() string
+}
+
+type GoProgrammer struct {
+}
+
+func (g *GoProgrammer) WriteHelloWorld() string {
+	return "fmt.Println(\"Hello World\")"
+}
+
+// 1. not depend on interface
+// 2. can declare interface at user package
+
+func TestClient(t *testing.T) {
+	// var p Programmer  -- should merge variable declaration with assignment on next line (S1021)
+	// p = new(GoProgrammer)
+	var p Programmer = new(GoProgrammer)
+	t.Log(p.WriteHelloWorld())
+}
