e90ba0c5bf2b711cb87eea2b3818d340775cdaa4
thomas <thomas.huan@beacora.com>
2022-08-16 09:18:31 +0000

    goroutine

--- others/goroutine/goroutine_test.go
+++ others/goroutine/goroutine_test.go
@@ -2,6 +2,7 @@ package goroutine_test
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
 	"time"
 )
@@ -18,4 +19,44 @@ func TestGoroutineCount(t *testing.T) {
 	time.Sleep(time.Millisecond * 2)
 	t.Log("hello world")
 	time.Sleep(time.Millisecond * 5)
+}
+
+func TestRun(t *testing.T) {
+	for i := 0; i < 1000; i++ {
+		go func(i int) {
+			for {
+				fmt.Printf("hello from goroutine %d\n", i)
+			}
+		}(i)
+	}
+	time.Sleep(time.Millisecond)
+}
+func TestWithoutIO(t *testing.T) {
+	var a [10]int
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			for {
+				a[i]++
+				runtime.Gosched()
+			}
+		}(i)
+
+	}
+	time.Sleep(time.Millisecond)
+	fmt.Println(a)
+}
+
+func TestWithoutIO2(t *testing.T) {
+	var a [10]int
+	for i := 0; i < 10; i++ {
+		go func(i int) { // race condition , 数据访问冲突
+			for {
+				a[i]++
+				runtime.Gosched()
+			}
+		}(i)
+
+	}
+	time.Sleep(time.Millisecond)
+	fmt.Println(a)
 }
