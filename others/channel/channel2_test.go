cc46892c92039a9be35060709ed433390ed53ef3
thomas <thomas.huan@beacora.com>
2022-08-16 09:50:48 +0000

    create and return channel

--- others/channel/channel2_test.go
+++ others/channel/channel2_test.go
@@ -6,17 +6,21 @@ import (
 	"time"
 )
 
-func worker(id int, c chan int) {
-	for {
-		fmt.Printf("worker %d received %d\n", id, <-c)
-	}
+func createWorker(id int) chan int {
+	c := make(chan int)
+	go func() {
+		for {
+			fmt.Printf("worker %d received %d\n", id, <-c)
+		}
+	}()
+
+	return c
 }
 
 func chanDemo() {
 	var channels [10]chan int
 	for i := 0; i < 10; i++ {
-		channels[i] = make(chan int)
-		go worker(i, channels[i])
+		channels[i] = createWorker(i)
 	}
 
 	for i := 0; i < 10; i++ {
