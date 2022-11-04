eae4a91f2ddcce2bab14d1545f67b484b681484d
thomas <thomas.huan@beacora.com>
2022-11-04 02:40:17 +0000

    select

--- others/goroutine/select/002/select.go
+++ others/goroutine/select/002/select.go
@@ -19,16 +19,28 @@ func generator() chan int {
 	return out
 }
 
+func worker(id int, c chan int) {
+	for n := range c {
+		fmt.Printf("worker %d received %d\n", id, n)
+	}
+}
+
+func createWorker(id int) chan<- int {
+	c := make(chan int)
+	go worker(id, c)
+	return c
+}
+
+// will block after chand send and then do w <- n, no so good
 func main() {
 	var c1, c2 = generator(), generator()
-
+	w := createWorker(0)
 	for {
 		select {
 		case n := <-c1:
-			fmt.Println("Received from c1:", n)
+			w <- n
 		case n := <-c2:
-			fmt.Println("Received from c2:", n)
-
+			w <- n
 		}
 	}
 
