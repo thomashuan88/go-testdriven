e4eafa71581a48db5094ca3dbc5d5ea36c7c49fa
thomas <thomas.huan@beacora.com>
2022-11-07 11:02:06 +0000

    walk

--- others/ago/maze/maze.go
+++ others/ago/maze/maze.go
@@ -27,14 +27,38 @@ type point struct {
 	i, j int
 }
 
-// func walk(maze [][]int, start, end point) {
-// 	steps := make([][]int, len(maze))
-// 	for i := range steps {
-// 		steps[i] = make([]int, len(maze[i]))
-// 	}
+func (p point) add(r point) point {
+	return point{p.i + r.i, p.j + r.j}
+}
 
-// 	Q := []point{start}
-// }
+func (p point) at(grid [][]int) (int, bool) {
+	return grid[p.i][p.j]
+}
+
+var dirs = [4]point{
+	{-1, 0}, {0, -1}, {1, 0}, {0, 1}}
+
+func walk(maze [][]int, start, end point) {
+	steps := make([][]int, len(maze))
+	for i := range steps {
+		steps[i] = make([]int, len(maze[i]))
+	}
+
+	Q := []point{start}
+
+	for len(Q) > 0 {
+		cur := Q[0]
+		Q = Q[1:]
+
+		for _, dir := range dirs {
+			next := cur.add(dir)
+
+			// maze at next is 0
+			// and steps at next is 0
+			// and next != start
+		}
+	}
+}
 
 func main() {
 	maze := readMaze("maze/maze.in")
@@ -46,5 +70,5 @@ func main() {
 		fmt.Println()
 	}
 
-	// walk(maze, point{0, 0}, point{len(maze) - 1, len(maze[0]) - 1})
+	walk(maze, point{0, 0}, point{len(maze) - 1, len(maze[0]) - 1})
 }
