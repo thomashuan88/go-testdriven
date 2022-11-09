ecc6453155180b8ca45c1c914b9c5e6aabac71ce
thomas <thomas.huan@beacora.com>
2022-11-09 07:45:41 +0000

    omitempty

--- others/json/main.go
+++ others/json/main.go
@@ -7,7 +7,7 @@ import (
 
 type Order struct {
 	ID         string  `json:"id"`
-	Name       string  `json:"name"`
+	Name       string  `json:"name,omitempty"`
 	Quantity   int     `json:"quantity"`
 	TotalPrice float64 `json:"total_price"`
 }
@@ -15,7 +15,6 @@ type Order struct {
 func main() {
 	o := Order{
 		ID:         "1234",
-		Name:       "learn go",
 		Quantity:   3,
 		TotalPrice: 30,
 	}
